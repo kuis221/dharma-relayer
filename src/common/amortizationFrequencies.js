31960a4274551fd733fa4c9ceb92efe172049127
Evgeniy Loginov <loginov_evgeniy@tut.by>
2018-03-31 19:47:00 +0300

    use dharma amortization units as a constant

--- src/common/amortizationFrequencies.js
+++ src/common/amortizationFrequencies.js
@@ -1,4 +1,14 @@
-export const END = 'End Of Loan Term';
-export const DAILY = 'Daily';
-export const WEEKLY = 'Weekly';
-export const MONTHLY = 'Monthly';
+export const RELAYER_AMORTIZATION_FREQUENCIES = {
+  END: 'End Of Loan Term',
+  DAILY: 'Daily',
+  WEEKLY: 'Weekly',
+  MONTHLY: 'Monthly'
+};
+
+export const DHARMA_AMORTIZATION_UNITS = {
+  HOURS: 'hours',
+  DAYS: 'days',
+  WEEKS: 'weeks',
+  MONTHS: 'months',
+  YEARS: 'years'
+};
